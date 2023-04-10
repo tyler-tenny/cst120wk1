e2b1879a16b169683027184b85febe0fd7065925
Tyler Tenny <tyler.tenny@oit.edu>
2023-04-10 15:26:38 -0700

    added comments to hw code

--- homework1.c
+++ homework1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
+//function to sum variables
 int sum (const int * a, const int * b) {
   int result = (*a) + (*b);
   return result;
 }
 
+//function to multiply variables
 int product (const int * a, const int * b) {
   int result = (*a) * (*b);
   return result;
 }
 
+//function to collect number of digits through division
 int digits (int a) {
   int result = 0;
   while (a != 0) {
@@ -19,6 +22,7 @@ int digits (int a) {
   return result;
 }
 
+//function to get input from user, and reject if it is not positive
 void collectInput (int * input) {
   printf ("Please enter a positive integer: ");
   scanf ("%d", input);
@@ -31,16 +35,20 @@ void collectInput (int * input) {
 
 int main() {
   char userCont = 'y';
+  //set up continue loop
   while (userCont == 'y') {
     int input1 = 0, input2 = 0;
+    //collecting inputs from user
     collectInput (&input1);
     collectInput (&input2);
 
+    //printing function results for user
     printf ("The sum of %d and %d is: %d \n", input1, input2, sum (&input1, &input2));
     int inputProduct = product (&input1, &input2);
     printf ("The product of %d and %d is: %d \n", input1, input2, inputProduct);
     printf ("%d has %d digits. \n", inputProduct, digits(inputProduct));
 
+    //ask for and collect continue
     printf ("Please enter \'y\' to continue: ");
     scanf (" %c", &userCont);
   }
